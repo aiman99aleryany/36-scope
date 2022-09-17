f032c75ff429d44d27ed3a9501e1e30fd762d3bb
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-17 22:13:21 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,24 @@
+// If you try to run this function, you'll get an error. Fix the function and assign a global scope to the variable sport. How can we do this?
+
+// function canPlay() {
+// let sport = " Football";
+
+// if (true) {
+// let personName = "Cosimo";
+// }
+
+// console.log(personName + sport);
+// }
+
+// canPlay();
+
+let sport = " Football";
+
+function canPlay() {
+  let personName = null;
+  if (true) {
+    personName = "Cosimo";
+  }
+  console.log(personName + sport);
+}
+canPlay();
